bd7214f5589de208a246a4de416c45d6b9bf3d06
AndrewVreeland <andrewvreeland@gmail.com>
2023-04-24 11:16:24 -0400

    finished code challaenge prepping for turn in

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -1,9 +1,64 @@
+
 package datastructures.linkedlist;
 
-public class LinkedList
-{
-  public String toString()
-    {
-        return "null";  // TODO: implement me
+public class LinkedList{
+  static Node head = null;
+// Node tail =null; optional
+
+
+  public static void insertFront(int value){
+    // TODO: implement me!
+    // add a new node as the head of the LinkedList
+    Node  newNode = new Node(value);
+  if (head == null){
+    head = newNode;
+  }else{
+    newNode.next = head;
+    head = newNode;
+  }
+  }
+
+  public static boolean includes(int value){
+// TODO: implement me!
+    // tell whether or not there is a node with the given value in the linked list
+    Node temp = head;
+    while (temp != null) {
+      if (temp.value == value) {
+        return true;
+      }
+      temp = temp.next;
     }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    // Iterate over linked list and return the data as a string
+    StringBuilder result = new StringBuilder();
+    Node temp = head;
+    while (temp != null) {
+      result.append(temp.value);
+      if (temp.next != null) {
+        result.append("->");
+      }
+      temp = temp.next;
+    }
+    return result.toString();
+  }
+  public static void main(String[] args) {
+    // Example usage
+    LinkedList myLL = new LinkedList();
+    insertFront(5);
+    insertFront(4);
+    insertFront(3);
+    insertFront(2);
+    insertFront(1);
+
+    System.out.println(myLL); // Output: 1->2->3->4->5
+    System.out.println(includes(3)); // Output: true
+    System.out.println(includes(6)); // Output: false
+  }
 }
+
+//LinkedList myLL - new LinkedList();
+//myLL.insertFront(5);
