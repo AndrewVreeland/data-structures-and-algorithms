dcb0cf67990a48bd89838d6469c336f9e98a8d56
AndrewVreeland <andrewvreeland@gmail.com>
2023-05-11 02:49:18 -0400

    white board is done I feel good about completeing this one it is just late and I am tired

--- java/datastructures/lib/src/main/java/codechallenges/binaryTree/BinarySearchTree.java
+++ java/datastructures/lib/src/main/java/codechallenges/binaryTree/BinarySearchTree.java
@@ -2,13 +2,38 @@ package codechallenges.binaryTree;
 
 public class BinarySearchTree extends BinaryTree {
 
-
-public void add(Integer value, Node node){
-
-  Node topNode = new Node<>(10);
-  if(node.left!=null) {
-    postOrder(node, node.left);
+Node root;
+public void add(Integer value){
+Node newNode = new Node(value);
+if(root == null){
+  root = newNode;
+}
+Node currentNode = root;
+while(currentNode != null){
+  if (value<currentNode.value){
+    if(currentNode.left == null){
+      currentNode.left = newNode;
+      return;
+    }
+currentNode = currentNode.left;
+  } else if (value>currentNode.value) {
+    if(currentNode.right == null){
+      currentNode.right = newNode;
+      return;
+    }
+    currentNode=currentNode.right;
+  }else {
+    return;
   }
 }
+}
 
+public static class Node{
+  int value;
+  Node left;
+  Node right;
+  public Node(int value){
+    this.value = value;
+  }
+}
 }
